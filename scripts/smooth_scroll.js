d30c7183fa112b666fbada4433c91b9dc7d9fc21
Milouu <milan.roustan@gmail.com>
2019-06-05 19:56:33 +0200

    :sparkles: Adding smooth scrolling on scroll events

--- scripts/smooth_scroll.js
+++ scripts/smooth_scroll.js
@@ -13,6 +13,12 @@ $(document).ready(function() {
     // Checks if the user is scrolling down
     if(currentScrollTop > oldScrollTop) {
 
+      /**
+       * UNCOMMENT FOR SMOOTH SCROLLING
+       */
+      // $path = $('#homepage_first_section').offset().top;
+      // $('html, body').animate({ scrollTop:$path }, 1000);
+
       // Displays backToTop when passing the header mid-point
       if(currentScrollTop >= $('#homepage_header').outerHeight(true)/2) {
         $('.backToTop').addClass('active');
@@ -20,6 +26,12 @@ $(document).ready(function() {
     
     // Checks if the user is scrolling up
     } else {
+      
+      /**
+       * UNCOMMENT FOR SMOOTH SCROLLING
+       */
+      // $path = $('#homepage_header').offset().top;
+      // $('html, body').animate({ scrollTop:$path }, 1000);
 
       // Hides backToTop button when sliding higher than the header mid-point
       if(currentScrollTop <= $('#homepage_header').outerHeight(true)/2) {
@@ -32,6 +44,13 @@ $(document).ready(function() {
 
   // Scrolls back to top of the page when click backToTop button
   $('.backToTop').click(function() {
+    // Stops scrolling events from interfering with backToTop animation (useful if smooth scrolling enabled)
+    $('html, body').stop(true, false);
     $('html, body').animate({ scrollTop: 0 }, 1000);
+  });
+
+  // Stops mousewheel events from interfering with smoothscroll (useful if smooth scrolling enabled)
+  $(window).bind('mousewheel', function() {
+    $('html, body').stop(true, false);
   });
 })
