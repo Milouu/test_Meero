6abcac52cfc4428bf06903f58aa33f07bf263d6e
Milouu <milan.roustan@gmail.com>
2019-06-05 20:07:34 +0200

    :iphone: Add responsive design to slider

--- scripts/slick_slider.js
+++ scripts/slick_slider.js
@@ -8,5 +8,30 @@ $(document).ready(function(){
     slidesToScroll: 1,
     lazyLoad: 'ondemand',
     cssEase:'linear',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
